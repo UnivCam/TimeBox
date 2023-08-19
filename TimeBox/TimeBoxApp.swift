a581ad1297ce9c625b678127afa2847ac9313b4f
junyng <pom0894@naver.com>
2023-08-19 14:46:26 +0900

    Add initial project

---
+++ TimeBox/TimeBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBoxApp.swift
+//  TimeBox
+//
+//  Created by junyng on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct TimeBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
