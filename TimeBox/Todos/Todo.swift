77fd8df7619d4fdc7605bea8d5b9745c261832b0
junyng <pom0894@naver.com>
2023-08-19 17:21:09 +0900

    Implement 'Todos' feature

---
+++ TimeBox/Todos/Todo.swift
@@ -0,0 +1,45 @@
+//
+//  Todo.swift
+//  TimeBox
+//
+//  Created by junyng on 2023/08/19.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct Todo: Reducer {
+    struct State: Equatable, Identifiable {
+        let id: UUID = UUID()
+        @BindingState var description: String = ""
+        @BindingState var hasPriority: Bool = false
+    }
+    
+    enum Action: BindableAction, Equatable, Sendable {
+        case binding(BindingAction<State>)
+    }
+    
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+    }
+}
+
+struct TodoView: View {
+    let store: StoreOf<Todo>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            HStack {
+                Button {
+                    viewStore.$hasPriority.wrappedValue.toggle()
+                } label: {
+                    Image(systemName: viewStore.hasPriority ? "checkmark.circle" : "circle")
+                }
+                .buttonStyle(.plain)
+                
+                TextField("Write down your Todo..", text: viewStore.$description)
+            }
+            .foregroundColor(viewStore.hasPriority ? .orange : nil)
+        }
+    }
+}
---
+++ TimeBox/Todos/Todos.swift
@@ -0,0 +1,65 @@
+//
+//  Todos.swift
+//  TimeBox
+//
+//  Created by junyng on 2023/08/19.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct Todos: Reducer {
+    struct State: Equatable {
+        var todos: IdentifiedArrayOf<Todo.State> = []
+    }
+    
+    enum Action: Equatable {
+        case addTodoButtonTapped
+        case delete(IndexSet)
+        case todo(id: Todo.State.ID, action: Todo.Action)
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .addTodoButtonTapped:
+                state.todos.append(Todo.State())
+                return .none
+            case let .delete(indexSet):
+                for index in indexSet {
+                    state.todos.remove(id: state.todos[index].id)
+                }
+                return .none
+            case .todo:
+                return .none
+            }
+        }
+        .forEach(\.todos, action: /Action.todo(id:action:)) {
+            Todo()
+        }
+    }
+}
+
+struct TodosView: View {
+    let store: StoreOf<Todos>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            List {
+                ForEachStore(
+                    self.store.scope(state: \.todos, action: Todos.Action.todo(id:action:))
+                ) {
+                    TodoView(store: $0)
+                }
+                .onDelete { viewStore.send(.delete($0)) }
+                
+                Button {
+                    viewStore.send(.addTodoButtonTapped)
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+            }
+        }
+    }
+}
